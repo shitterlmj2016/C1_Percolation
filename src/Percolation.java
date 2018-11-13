d851a161c0e2b8c51e2a9c786ce6a17d4622fb5e
shitterlmj2016 <915930987@qq.com>
2018-11-13 01:00:38 -0500

    V2 Construction Function: 还有回流和其他的Bug

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,16 +1,16 @@
-import edu.princeton.cs.algs4.QuickUnionUF;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
-    private QuickUnionUF uf;
-    private static int length;
+    private int length;
+    private WeightedQuickUnionUF uf;
     private int[] openSite;
+    private int openCount;
 
-    public Percolation(int n) throws java.lang.IllegalArgumentException {   //Check legal
+    public Percolation(int n) {   //Check legal
         if (n < 1)
             throw new IllegalArgumentException("info " + n + " is smaller than 1");
-        length=n;
+        length = n;
         int total = n * n + 2;
         openSite = new int[total];
         for (int i = 0; i < openSite.length; i++) {
@@ -19,52 +19,109 @@ public class Percolation {
         openSite[0] = 1;
         openSite[total - 1] = 1;
         //Create a recorder
-        uf=new QuickUnionUF(total);
-        for(int i=1; i<length;i++){
-            uf.union(0,i);
-            uf.union(total-1,total-1-i);
+        uf = new WeightedQuickUnionUF(total);
+        for (int i = 1; i <= length; i++) {
+            uf.union(0, i);
+            uf.union(total - 1, total - 1 - i);
         }
         //connect all items
     }
 
-    ;               // create n-by-n grid, with all sites blocked
+    // create n-by-n grid, with all sites blocked
 
+    private int getIndex(int row, int col) {
+        return (row - 1) * length + col;
+    }
 
-    public void open(int row, int col) throws java.lang.IllegalArgumentException{
+    public static void main(String[] args) {
+        Percolation p = new Percolation(1);
+        Percolation p2 = new Percolation(2);
+        System.out.println(p2.length);
+        System.out.println(p.isFull(1, 1));
+        System.out.println(p.percolates());
+        p.open(1, 1);
+        System.out.println(p.percolates());
+//        p.open(1,2);
+//        System.out.println(p.percolates());
+//        p.open(2,2);
+//        p.open(3,2);
+        System.out.println(p.numberOfOpenSites());
+
+        //System.out.println(p.uf.connected(getIndex(1,1),1));*/
+
+    }// test client (optional)
+
+    public void open(int row, int col) {
         //Check if input is legal
+        if (row < 1 || row > length || col > length || col < 1) {
+            throw new IllegalArgumentException("info " + row + " or " + col + " is smaller than 1");
+        }
+        int index = getIndex(row, col);
+        //需要检测是否已经open
+        if (openSite[index] != 1) {
+            openSite[index] = 1;
+            openCount++;
+        }
+
+        //UP
+        if (row > 1) {
+            if (isOpen(row - 1, col)) {
+                int upIndex = getIndex(row - 1, col);
+                uf.union(upIndex, index);
+            }
+        }
+        //DOWN
+        if (row < length) {
+            if (isOpen(row + 1, col)) {
+                int downIndex = getIndex(row + 1, col);
+                uf.union(downIndex, index);
+            }
+        }
+
+        //LEFT
+        if (col > 1) {
+            if (isOpen(row, col - 1)) {
+                int leftIndex = getIndex(row, col - 1);
+                uf.union(leftIndex, index);
+            }
+        }
+
+        //RIGHT
+        if (col < length) {
+            if (isOpen(row, col + 1)) {
+                int rightIndex = getIndex(row, col + 1);
+                uf.union(rightIndex, index);
+            }
+        }
 
     }// open site (row, col) if it is not open already
 
-    public boolean isOpen(int row, int col)  // is site (row, col) open?
+    public boolean isOpen(int row, int col)// is site (row, col) open?
     {
-        return true;
+        if (row < 1 || row > length || col > length || col < 1) {
+            throw new IllegalArgumentException("info " + row + " or " + col + " is smaller than 1");
+        }
+        return (openSite[getIndex(row, col)] == 1);
     }
 
     public boolean isFull(int row, int col) {
-        return true;
+        if (row < 1 || row > length || col > length || col < 1) {
+            throw new IllegalArgumentException("info " + row + " or " + col + " is smaller than 1");
+        }
+        int index = getIndex(row, col);
+        return (isOpen(row, col) && uf.connected(0, index));
     }// is site (row, col) full?
 
     public int numberOfOpenSites() {
-        return 0;
+        return openCount;
     }// number of open sites
 
     public boolean percolates() {
-        return true;
+        if(length==1)
+        {
+            return openCount==1;
+        }
+        return uf.connected(0, length * length + 1);
     }// does the system percolate?
 
-    public static int getIndex(int row, int col){
-        return (row-1)*length+col;
-    }
-
-
-    public static void main(String[] args) {
-        WeightedQuickUnionUF wuf = new WeightedQuickUnionUF(8);
-        wuf.union(0, 1);
-        wuf.union(0, 2);
-
-        int a = 1;
-
-        Percolation p = new Percolation(4);
-        System.out.println(getIndex(2,3));
-    }// test client (optional)
 }
--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,30 +1,71 @@
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
-import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class PercolationStats {
-    public PercolationStats(int n, int trials)
-    {
+    private double[] records;
+    private int length;
+    private int trials;
+    private double mean;
+    private double stddev;
 
+    public PercolationStats(int n, int trials) {
+        if (n < 1 || trials < 1)
+            throw new java.lang.IllegalArgumentException("info " + n + " is smaller than 1");
+        this.trials = trials;
+        this.length = n;
+        records = new double[trials];
+
+        for (int i = 0; i < trials; i++) {
+            Percolation p = new Percolation(length);
+
+            while (!p.percolates()) {
+                int row = StdRandom.uniform(1, length + 1);
+                int col = StdRandom.uniform(1, length + 1);
+                p.open(row, col);
+                //System.out.println(p.numberOfOpenSites());
+            }
+
+            records[i] = p.numberOfOpenSites()/(double)(n*n);
+        }
+
+
+        mean=StdStats.mean(records);
+        stddev=StdStats.stddev(records);
     }// perform trials independent experiments on an n-by-n grid
-    public double mean()
-    {
-        return 0;
-    }// sample mean of percolation threshold
-    public double stddev()
-    {
-        return 1;
-    }// sample standard deviation of percolation threshold
-    public double confidenceLo()
-    {
-        return 1;
-    }// low  endpoint of 95% confidence interval
-    public double confidenceHi()
-    {
-        return 1;
-    }// high endpoint of 95% confidence interval
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+
+        int n = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);
+        PercolationStats stats = new PercolationStats(n, trials);
+        double x = stats.mean();
+        double s = stats.stddev();
+        double low = stats.confidenceLo();
+        double hi = stats.confidenceHi();
+        System.out.printf("mean=%f\n", x);
+        System.out.printf("stddev=%f\n", s);
+        System.out.printf("%f %f\n", low, hi);
+
 
     }// test client (described below)
+
+    public double mean() {
+        if(mean!=0)
+            return mean;
+        return StdStats.mean(records);
+    }// sample mean of percolation threshold
+
+    public double stddev() {
+        if(stddev!=0)
+            return stddev;
+        return StdStats.stddev(records);
+    }// sample standard deviation of percolation threshold
+
+    public double confidenceLo() {
+        return mean()-1.96*stddev()/Math.sqrt(length);
+    }// low  endpoint of 95% confidence interval
+
+    public double confidenceHi() {
+        return mean()-1.96*stddev()/Math.sqrt(length);
+    }// high endpoint of 95% confidence interval
 }
